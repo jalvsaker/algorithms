f46b07bd63435db91b27be51b840d451bb52e945
jalvsaker <jalvsaker@users.noreply.github.com>
2022-02-18 13:15:21 +0100

    ex03b

--- exercises/src/test/java/org/pg4200/ex03/OptimizedBubbleSortTest.java
+++ exercises/src/test/java/org/pg4200/ex03/OptimizedBubbleSortTest.java
@@ -67,4 +67,34 @@ public class OptimizedBubbleSortTest {
         System.out.println("opt: " + optimized + "  unopt: " + unoptimized);
         assertTrue(optimized <= unoptimized/2);
     }
+
+    @Test
+    public void testGameUserComparator(){
+
+        GameUser gu6 = new GameUser("Id 62", 1);
+        GameUser gu7 = new GameUser("Id 543", 12);
+        GameUser gu2 = new GameUser("Id 6", 456);
+        GameUser gu1 = new GameUser("Id 1", 500);
+        GameUser gu5 = new GameUser("Id 12", 1000);
+        GameUser gu4 = new GameUser("Id 89", 1000);
+        GameUser gu3 = new GameUser("Id 2", 5000);
+
+        GameUser[] GameUserArray = {gu1, gu2, gu3, gu4, gu5, gu6, gu7};
+
+        OptimizedBubbleSort sorter = new OptimizedBubbleSort();
+        sorter.sort(GameUserArray, new GameUserComparator(), false);
+
+        for (GameUser gu :
+                GameUserArray) {
+            System.out.println(gu.getUserId() + " + " + gu.getPoints());
+        }
+
+        assertEquals(gu6, GameUserArray[0]);
+        assertEquals(gu7, GameUserArray[1]);
+        assertEquals(gu2, GameUserArray[2]);
+        assertEquals(gu1, GameUserArray[3]);
+        assertEquals(gu5, GameUserArray[4]);
+        assertEquals(gu4, GameUserArray[5]);
+        assertEquals(gu3, GameUserArray[6]);
+    }
 }
