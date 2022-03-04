dbff30e14967bc2dbf227274f18c18b29a9e266e
jalvsaker <jalvsaker@users.noreply.github.com>
2022-03-04 13:10:15 +0100

    ex04e -- 3 first

---
+++ exercises/src/main/java/org/pg4200/ex04/TriangleClassification.java
@@ -0,0 +1,13 @@
+package org.pg4200.ex04;
+
+public class TriangleClassification {
+    public enum Classification {NOT_A_TRIANGLE, ISOSCELES, SCALENE, EQUILATERAL}
+
+    public static Classification classify(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0){
+            return Classification.NOT_A_TRIANGLE;
+        }
+
+        return null;
+    }
+}
---
+++ exercises/src/test/java/org/pg4200/ex04/TriangleClassificationTest.java
@@ -0,0 +1,24 @@
+package org.pg4200.ex04;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TriangleClassificationTest {
+
+    @Test
+    void negativeA(){
+        assertEquals(TriangleClassification.classify(-1, 0, 0), TriangleClassification.Classification.NOT_A_TRIANGLE);
+    }
+
+    @Test
+    void negativeB(){
+        assertEquals(TriangleClassification.classify(0,-1,0), TriangleClassification.Classification.NOT_A_TRIANGLE);
+    }
+
+    @Test
+    void negativeC(){
+        assertEquals(TriangleClassification.classify(0,0,-1), TriangleClassification.Classification.NOT_A_TRIANGLE);
+    }
+}
