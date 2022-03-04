4a82da7b6804ffda5206ccfc40c0dd57d2da3ea6
jalvsaker <jalvsaker@users.noreply.github.com>
2022-03-04 15:21:57 +0100

    ex04b -- rename cache

--- exercises/src/main/java/org/pg4200/ex04/FibonacciImplMemoized.java
+++ exercises/src/main/java/org/pg4200/ex04/FibonacciImplMemoized.java
@@ -8,7 +8,7 @@ public class FibonacciImplMemoized implements Fibonacci{
      * @throws IllegalArgumentException if n is negative
      */
 
-    int[] x = new int[100];
+    int[] cache = new int[100];
 
     @Override
     public int compute(int n) throws IllegalArgumentException {
@@ -26,18 +26,18 @@ public class FibonacciImplMemoized implements Fibonacci{
         int nmin2;
         int nmin1;
 
-        if (x[n-2] == 0){
+        if (cache[n-2] == 0){
             nmin2 = compute(n - 2);
-            x[n-2] = nmin2;
+            cache[n-2] = nmin2;
         } else {
-            nmin2 = x[n-2];
+            nmin2 = cache[n-2];
         }
 
-        if (x[n-1] == 0){
+        if (cache[n-1] == 0){
             nmin1 = compute(n - 1);
-            x[n-1] = nmin1;
+            cache[n-1] = nmin1;
         } else {
-            nmin1 = x[n-1];
+            nmin1 = cache[n-1];
         }
 
 
