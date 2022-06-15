8064c48b137b4790ffbc503e9e94f82945ecf807
jalvsaker <jalvsaker@users.noreply.github.com>
2022-06-15 10:03:30 +0200

    course program student

---
+++ exercises/src/main/java/org/pg4200/exam/ex04extraClasses/Course.java
@@ -0,0 +1,9 @@
+package org.pg4200.exam.ex04extraClasses;
+
+import java.util.HashMap;
+
+public class Course {
+    public String courseName;
+    public String courseCode;
+    public HashMap<Integer, Student> students;
+ }
---
+++ exercises/src/main/java/org/pg4200/exam/ex04extraClasses/Program.java
@@ -0,0 +1,12 @@
+package org.pg4200.exam.ex04extraClasses;
+
+import java.util.ArrayList;
+
+public class Program {
+    public String programName;
+    public ArrayList<Course> courses;
+
+    public ArrayList<Course> getCourses(){
+        return courses;
+    }
+}
